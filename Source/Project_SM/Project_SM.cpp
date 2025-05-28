91214d9d8f3a1fe2529512e8129b5da68d19425a
CJS <gksdidxornjs@naver.com>
2025-05-29 00:56:49 +0900

    프로젝트 생성

---
+++ Source/Project_SM/Project_SM.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Project_SM.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Project_SM, "Project_SM" );
